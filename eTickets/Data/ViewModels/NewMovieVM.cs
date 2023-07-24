b5cf8a535f59664bbe1b9425e70964393947251f
Mauricio Magalhaes <mauriciosmagalhaes@gmail.com>
2023-07-24 10:43:20 -0300

    add new movies to database

---
+++ eTickets/Data/ViewModels/NewMovieVM.cs
@@ -0,0 +1,43 @@
+﻿using eTickets.Data.Base;
+using eTickets.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class NewMovieVM
+    {
+        [Display(Name = "Movie Name")]
+        [Required(ErrorMessage = "Name is requered")]
+        public string Name { get; set; }
+        [Display(Name = "Movie Description")]
+        [Required(ErrorMessage = "Description is requered")]
+        public string Description { get; set; }
+        [Display(Name = "Price in $")]
+        [Required(ErrorMessage = "Price is requered")]
+        public double Price { get; set; }
+        [Display(Name = "Movie poster URL")]
+        [Required(ErrorMessage = "Movie poster URL is requered")]
+        public string ImageURL { get; set; }
+        [Display(Name = "Movie start date")]
+        [Required(ErrorMessage = "Start date is requered")]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "Movie end date")]
+        [Required(ErrorMessage = "End date is requered")]
+        public DateTime EndDate { get; set; }
+        [Display(Name = "Selecet a category")]
+        [Required(ErrorMessage = "Movie category is requered")]
+        public MovieCategoryEnum MovieCategory { get; set; }
+        [Display(Name = "Selecet actor(s)")]
+        [Required(ErrorMessage = "Movie actor(s) is requered")]
+        public List<int> ActorsIds { get; set; }
+        [Display(Name = "Selecet a cinema")]
+        [Required(ErrorMessage = "Movie cinema is requered")]
+        public int CinemaId { get; set; }
+        [Display(Name = "Selecet a producer")]
+        [Required(ErrorMessage = "Movie producer is requered")]
+        public int ProducerId { get; set; }
+    }
+}
