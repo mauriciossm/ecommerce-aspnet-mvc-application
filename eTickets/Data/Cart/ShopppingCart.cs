a4b32dcbf06bdf66603043624265e3dd52615827
Mauricio Magalhaes <mauriciosmagalhaes@gmail.com>
2023-08-10 10:09:45 -0300

    added shopping cart model

---
+++ eTickets/Data/Cart/ShopppingCart.cs
@@ -0,0 +1,28 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eTickets.Data.Cart
+{
+    public class ShopppingCart
+    {
+        public AppDbContext _context { get; set; }
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItemModel> ShoppingCartItems { get; set; }
+        public ShopppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItemModel> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems
+                .Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal() => _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId)
+                .Select(n => n.Movie.Price * n.Amount).Sum();
+            
+    }
+}
